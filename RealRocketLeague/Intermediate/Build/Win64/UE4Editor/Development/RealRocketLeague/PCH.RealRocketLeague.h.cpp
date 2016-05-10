69e2af251f4a0002972f4e66bd2cbe6dd7397dec
Sundanian <thomas.krog.horne@gmail.com>
2016-05-10 14:05:06 +0200

    Der er nu et BP til ball og goal. Intet kode endnu

--- RealRocketLeague/Intermediate/Build/Win64/UE4Editor/Development/RealRocketLeague/PCH.RealRocketLeague.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:\EA-Dania\4. Semester\Eksamens Projekt\RealRocketLeague\Source\RealRocketLeague\RealRocketLeague.h"
--- RealRocketLeague/Intermediate/Build/Win64/UE4Editor/Development/RealRocketLeague/RealRocketLeague.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_REALROCKETLEAGUE_H__
-#define __AUTO_REALROCKETLEAGUE_H__
-//Last Write: 10-05-2016 07:09:01 +00:00
-#include "D:\EA-Dania\4. Semester\Eksamens Projekt\RealRocketLeague\Source\RealRocketLeague\RealRocketLeague.h"
-#endif//__AUTO_REALROCKETLEAGUE_H__
